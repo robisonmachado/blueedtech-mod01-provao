b804c8a0737e364508be6f9abc9403ec2b86558d
Robison Pereira Machado <robisonpmachado@gmail.com>
2022-05-11 23:26:38 -0300

    exercício 4

--- exercicio-04.js
+++ exercicio-04.js
@@ -2,3 +2,21 @@ const prompt = require('prompt-sync')();
 
 console.log("PROVÃO BLUEEDTECH - CLASSE 16 - MÓDULO 1 - EXERCÍCIO 04");
 console.log("aluno: Robison Pereira Machado");
+
+
+console.log("\n\nDigite quantos casos de teste você gostaria de realizar: ")
+const totalCasosDeTeste = Number.parseInt(prompt());
+
+for (let index = 0; index < totalCasosDeTeste; index++) {
+    console.log("\n\nInforme a população da cidade A: ")
+    const populacaoCidadeA = Number.parseInt(prompt());
+
+    console.log("\n\nInforme a população da cidade B: ")
+    const populacaoCidadeB = Number.parseInt(prompt());
+
+    console.log("\n\nInforme o percentual de crescimento anual da população da cidade A: ")
+    const percentualCrescimentoCidadeA = Number.parseFloat(prompt());
+
+    console.log("\n\nInforme o percentual de crescimento anual da população da cidade B: ")
+    const percentualCrescimentoCidadeB = Number.parseFloat(prompt());
+}
